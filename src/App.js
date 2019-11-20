5449d2acb5e56e3fce5d1b3861f6c323a379d682
Bonnie Li <bonnie@hatchways.io>
2019-11-20 13:37:04 -0500

    get rid of material ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
