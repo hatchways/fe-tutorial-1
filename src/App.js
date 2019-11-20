bd90bed185d3f03f47ad6f980a33b54afe1dcb8f
Bonnie Li <bonnie@hatchways.io>
2019-11-19 21:03:06 -0500

    breakpoints

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import logo from "./logo.svg";
-// import "./App.css";
 
 const useStyles = makeStyles(theme => ({
   signUp: {
-    border: "1px solid black",
+    border: "1px solid white",
+    color: "white",
     borderRadius: 19,
-    margin: 15,
-    position: "absolute",
-    right: 0
+    margin: 15
+  },
+  signUpContainer: {
+    height: "100vh",
+    [theme.breakpoints.down("xs")]: {
+      flexFlow: "column-reverse"
+    }
   },
   leftContainer: {
     backgroundColor: "black",
@@ -26,12 +29,13 @@ function App() {
   const classes = useStyles();
 
   return (
-    <Grid container style={{ height: "100vh" }}>
-      <Grid item xs={6} className={classes.leftContainer}>
+    <Grid container className={classes.signUpContainer}>
+      <Grid item xs={12} sm={6} className={classes.leftContainer}>
         left container
-      </Grid>
-      <Grid item xs={6} className={classes.rightContainer}>
         <Button className={classes.signUp}>Sign up</Button>
+      </Grid>
+      <Grid item xs={12} sm={6} className={classes.rightContainer}>
+        right container
       </Grid>
     </Grid>
   );
