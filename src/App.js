0de2ff42c9d39f4e49c768069ad6acf7be911fc0
Bonnie Li <bonnie@hatchways.io>
2019-11-21 10:21:42 -0500

    Class name

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <div id="container">
-        <div id="left_container" class="half_container">
+        <div id="left_container" className="half_container">
           left container
           <a href="/signup" id="signup">
             sign up
           </a>
         </div>
-        <div id="right_container" class="half_container">
+        <div id="right_container" className="half_container">
           right container
         </div>
       </div>
