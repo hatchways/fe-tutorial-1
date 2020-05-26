4edd8aa7aa3af4823bdcc8825b0f55bd37a53435
Bonnie Li <bonnie@hatchways.io>
2020-05-26 12:48:46 -0400

    Change the breakpoints

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   signUp: {
     border: "1px solid white",
     color: "white",
     borderRadius: 19,
-    margin: 15
+    margin: 15,
   },
   signUpContainer: {
     height: "100vh",
+    // When it reaches the breakpoint at "xs" or smaller, then apply this CSS
     [theme.breakpoints.down("xs")]: {
-      flexFlow: "column-reverse"
-    }
+      flexFlow: "column-reverse",
+    },
   },
   leftContainer: {
     backgroundColor: "black",
-    color: "white"
+    color: "white",
   },
   rightContainer: {
-    position: "relative"
-  }
+    position: "relative",
+  },
 }));
 
 function App() {
@@ -30,11 +31,11 @@ function App() {
 
   return (
     <Grid container className={classes.signUpContainer}>
-      <Grid item xs={12} sm={6} className={classes.leftContainer}>
+      <Grid item xs={12} sm={12} md={6} className={classes.leftContainer}>
         left container
         <Button className={classes.signUp}>Sign up</Button>
       </Grid>
-      <Grid item xs={12} sm={6} className={classes.rightContainer}>
+      <Grid item xs={12} sm={12} md={6} className={classes.rightContainer}>
         right container
       </Grid>
     </Grid>
